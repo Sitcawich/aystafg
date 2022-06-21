4aa42f65b57c5cf6af27aefce3eb92557ef1484e
Miles Sitcawich <milessitcawich@gmail.com>
2022-06-20 23:47:49 -0500

    found redundancy in Game and updated Header

--- quiz-app/src/App.js
+++ quiz-app/src/App.js
@@ -2,8 +2,6 @@ import './App.css';
 import Header from './Header';
 import Game from './Game';
 
-
-
 function App() {
   return (
     <div>
--- quiz-app/src/Game.js
+++ quiz-app/src/Game.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Question from './Question';
 import Answer from './Answer';
 import Score from './Score';
+
 export default class Game extends React.Component{
     constructor(props){
         super(props);
@@ -11,29 +12,32 @@ export default class Game extends React.Component{
         };
     }
 
-    renderQuestion(){
-        return(
-            <Question questionNumber = {this.state.questionNumber}/>
-        );
-    }
-    renderAnswer(){
-        return(
-            <Answer questionNumber = {this.state.questionNumber}/>
-        );
-    }
-    renderScore() {
-        return (
-            <Score questionNumber = {this.state.questionNumber} score={this.state.score} />
-        );
-    }
+    // renderQuestion(){
+    //     return(
+    //         <Question questionNumber = {this.state.questionNumber}/>
+    //     );
+    // }
+    // renderAnswer(){
+    //     return(
+    //         <Answer questionNumber = {this.state.questionNumber}/>
+    //     );
+    // }
+    // renderScore() {
+    //     return (
+    //         <Score questionNumber = {this.state.questionNumber} score={this.state.score} />
+    //     );
+    // }
+
+    // redundant code above just put <Question /> into the render method without creating helper methods
+
     render(){
         return(
             <div className = 'panel'>
                 <h3>
-                    {this.renderQuestion()}
+                <Question questionNumber = {this.state.questionNumber}/>
                 </h3>
-                <div>{this.renderAnswer()}</div>
-                <div>{this.renderScore()}</div>
+                <div><Answer questionNumber = {this.state.questionNumber}/></div>
+                <div><Score questionNumber = {this.state.questionNumber} score={this.state.score} /></div>
                
             </div>
         );

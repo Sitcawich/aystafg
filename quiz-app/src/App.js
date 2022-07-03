685455b1e0b6b8f3f76ad104b7ac7a78efb2ab7d
Miles Sitcawich <milessitcawich@gmail.com>
2022-07-03 17:51:17 -0500

    added footer

--- quiz-app/src/App.js
+++ quiz-app/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './Header';
 import Game from './Game';
+import Footer from './Footer';
 
 function App() {
   return (
     <div>
       <Header />
       <Game />
+      <Footer />
     </div>
   );
 }

8b0af6fba042b2d003636ff6f0545f09f9657288
rydersitcawich <rydersitcawich@gmail.com>
2022-07-12 23:54:24 -0500

    changed link back

--- src/Footer.js
+++ src/Footer.js
@@ -4,7 +4,7 @@ export default class Footer extends React.Component {
     render () {
         return (
             <div class="footer">
-                <a href="https://i.pinimg.com/originals/b2/78/91/b2789149b65477f4c6aed69c8a8e3061.png" target="_blank" >About the Creator</a>
+                <a href="https://www.linkedin.com/in/ryder-sitcawich" target="_blank" >About the Creator</a>
             </div>
         );
     }

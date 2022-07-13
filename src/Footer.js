d31d20eabddfb0d9349229849d328147ec7020e1
rydersitcawich <rydersitcawich@gmail.com>
2022-07-13 00:05:57 -0500

    updated footer for both links

--- src/Footer.js
+++ src/Footer.js
@@ -3,8 +3,7 @@ import React from 'react';
 export default class Footer extends React.Component {
     render () {
         return (
-            <div class="footer">
-                <a href="https://www.linkedin.com/in/ryder-sitcawich" target="_blank" >About the Creator</a>
+            <div class="footer">About the Creators: <a href="https://www.linkedin.com/in/ryder-sitcawich-7394031a6/" target="_blank" >Ryder Sitcawich</a> & <a href="https://akatsuki-slayer.github.io" target="_blank">Miles Sitcawich</a>
             </div>
         );
     }

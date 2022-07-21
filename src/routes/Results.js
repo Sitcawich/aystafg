6a71afac1d61c2a2801099f8fde40260872d3fea
rydersitcawich <rydersitcawich@gmail.com>
2022-07-21 16:04:43 -0500

    fixed bug when user gets score of 0 on results page

--- src/routes/Results.js
+++ src/routes/Results.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
 const Results = () => {
+  let score = sessionStorage.getItem("score");
+  if (score === null){
+    score = 0;
+  }
   return (
     <div className = 'results-container'>
-      <h2 className = 'results-text'>Thanks for playing! <br/> Here was your final score: {sessionStorage.getItem("score")}/{sessionStorage.getItem("numAnswered")}</h2>
+      <h2 className = 'results-text'>Thanks for playing! <br/> Here was your final score: {score}/{sessionStorage.getItem("numAnswered")}</h2>
     </div>
     
   )
